dc58da943d491ae8778d0737eb8918ea390b82db
fujimoto kyosuke <kyoro.f@gmail.com>
2020-03-16 08:40:16 +0900

    Move constants to config

--- src/main/kotlin/Config.kt
+++ src/main/kotlin/Config.kt
@@ -1,6 +1,7 @@
 package com.github.lusingander
 
 object Config {
+    fun githubApiUrl(): String = "https://api.github.com/graphql"
     fun port(): Int = 7000
-    fun gitHubApiToken(): String? = ProcessBuilder().environment()["GITHUB_API_TOKEN"]
+    fun githubApiToken(): String? = ProcessBuilder().environment()["GITHUB_API_TOKEN"]
 }
--- src/main/kotlin/github/GraphQLClient.kt
+++ src/main/kotlin/github/GraphQLClient.kt
@@ -9,17 +9,13 @@ class GraphQLClient(
     private val id: String
 ) {
 
-    companion object {
-        private const val GITHUB_API_URL = "https://api.github.com/graphql"
-    }
-
     fun request(): String {
         return executePost(UserPullRequests.queryJson(this.id))
     }
 
     private fun executePost(query: String): String {
-        val (_, _, result) = Fuel.post(GITHUB_API_URL)
-            .header(Headers.AUTHORIZATION, "bearer ${Config.gitHubApiToken()}")
+        val (_, _, result) = Fuel.post(Config.githubApiUrl())
+            .header(Headers.AUTHORIZATION, "bearer ${Config.githubApiToken()}")
             .body(query)
             .responseString()
 
