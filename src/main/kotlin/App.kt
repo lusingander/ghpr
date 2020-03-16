9ed25b43c8fa58693ea3b9a8cba2d82b92b41fc2
fujimoto kyosuke <kyoro.f@gmail.com>
2020-03-16 23:08:52 +0900

    Add view

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -2,12 +2,12 @@ package com.github.lusingander
 
 import com.github.lusingander.github.GraphQLClient
 import io.javalin.Javalin
+import io.javalin.plugin.rendering.vue.VueComponent
 
 fun main() {
     with (Javalin.create()) {
-        get("/") { ctx ->
-            ctx.result("NOT IMPLEMENTED")
-        }
+        get("/") { ctx -> ctx.result("NOT IMPLEMENTED")}
+        get("/user/:id", VueComponent("<user-prs></user-prs>"))
         get("/api/user/:id") { ctx ->
             val id = ctx.pathParam("id")
             val response = GraphQLClient(id).request()
---
+++ src/main/resources/vue/views/user-prs.vue
@@ -0,0 +1,25 @@
+<template id="user-prs">
+    <div v-if="data">
+        {{ data }}
+    </div>
+    <div v-if="error">
+        {{ error }}
+    </div>
+</template>
+<script>
+    Vue.component("user-prs", {
+        template: "#user-prs",
+        data: () => ({
+            data: null,
+            error: null,
+        }),
+        created() {
+            let userId = this.$javalin.pathParams["id"];
+            axios.get("/api/user/" + userId).then(response => {
+                this.data = response.data;
+            }).catch(error =>
+                this.error = error
+            );
+        },
+    });
+</script>
