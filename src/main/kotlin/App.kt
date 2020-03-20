ede68f415612304c06463f7b679bcce0c89c99fc
fujimoto kyosuke <kyoro.f@gmail.com>
2020-03-21 00:04:59 +0900

    Add request logger

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -4,9 +4,15 @@ import com.github.lusingander.github.GraphQLClient
 import com.github.lusingander.github.GraphQLRequestException
 import io.javalin.Javalin
 import io.javalin.plugin.rendering.vue.VueComponent
+import org.slf4j.LoggerFactory
 
 fun main() {
+    val log = LoggerFactory.getLogger("app.AppKt")
+
     with (Javalin.create()) {
+        config.requestLogger { ctx, ms ->
+            log.info("${ctx.status()} url=${ctx.fullUrl()} time=$ms UA=${ctx.userAgent()} ")
+        }
         get("/", VueComponent("<user-search></user-search>"))
         get("/user/:id", VueComponent("<user-prs></user-prs>"))
         get("/api/user/:id") { ctx ->
