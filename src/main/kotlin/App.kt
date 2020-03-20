40a3c15a32b1f358e1b6b07cd4e970c45a562365
fujimoto kyosuke <kyoro.f@gmail.com>
2020-03-20 14:16:23 +0900

    Add index(search) page

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -6,7 +6,7 @@ import io.javalin.plugin.rendering.vue.VueComponent
 
 fun main() {
     with (Javalin.create()) {
-        get("/") { ctx -> ctx.result("NOT IMPLEMENTED")}
+        get("/", VueComponent("<user-search></user-search>"))
         get("/user/:id", VueComponent("<user-prs></user-prs>"))
         get("/api/user/:id") { ctx ->
             val id = ctx.pathParam("id")
---
+++ src/main/resources/vue/views/user-search.vue
@@ -0,0 +1,27 @@
+<template id="user-search">
+  <div>
+    <el-row :gutter="10">
+      <el-col :span="7" :offset="8">
+        <el-input placeholder="GitHub account ID" v-model="input" @keydown.enter.native="search"></el-input>
+      </el-col>
+      <el-col :span="1">
+        <el-button icon="el-icon-search" circle @click="search"></el-button>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<script>
+  Vue.component("user-search", {
+    template: "#user-search",
+    data: () => ({
+      input: ''
+    }),
+    methods: {
+      search() {
+        window.location = `/user/${this.input}`
+      }
+    }
+  });
+</script>
+<style>
+</style>
