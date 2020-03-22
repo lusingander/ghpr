225f79fd926423eb1a6cb21c518665c94067173a
fujimoto kyosuke <kyoro.f@gmail.com>
2020-03-22 10:46:23 +0900

    Organize

--- src/main/kotlin/github/api/UserPullRequestsResponse.kt
+++ src/main/kotlin/github/api/UserPullRequestsResponse.kt
@@ -2,111 +2,114 @@ package com.github.lusingander.github.api
 
 import com.github.lusingander.github.prs.UserPullRequestsResponse as GitHubResponse
 
-private const val GITHUB_BASE_URL = "https://github.com/"
-
-fun buildUserResponseFrom(src: GitHubResponse): UserPullRequestsResponse {
-    val prs = src.data.user.pullRequests
-    return UserPullRequestsResponse(
-        owners = buildOwners(prs.nodes),
-        cursors = buildCursors(prs.edges),
-        totalCount = prs.totalCount
-    )
-}
-
-private fun buildCursors(edges: List<GitHubResponse.Edge>): Cursors {
-    return Cursors(edges.first().cursor, edges.last().cursor)
-}
-
-private fun buildOwners(nodes: List<GitHubResponse.PullRequestNode>): List<Owner> {
-    return nodes.groupBy {
-        it.repository.owner.login
-    }.map {
-        buildOwner(it.key, it.value)
-    }
-}
-
-private fun buildOwner(name: String, prs: List<GitHubResponse.PullRequestNode>): Owner {
-    val url = GITHUB_BASE_URL + name
-    return Owner(
-        name,
-        url,
-        buildRepositories(prs, url)
-    )
-}
-
-private fun buildRepositories(prs: List<GitHubResponse.PullRequestNode>, ownerUrl: String): List<Repository> {
-    return prs.groupBy {
-        it.repository.name
-    }.map {
-        buildRepository(it.key, it.value, ownerUrl)
-    }
-}
-
-private fun buildRepository(name: String, prs: List<GitHubResponse.PullRequestNode>, ownerUrl: String): Repository {
-    val url = "$ownerUrl/$name"
-    val repo = prs.first().repository
-    return Repository(
-        name = name,
-        description = repo.description,
-        url = url,
-        watchers = repo.watchers.totalCount,
-        stars = repo.stargazers.totalCount,
-        forks = repo.forkCount,
-        langName = repo.primaryLanguage.name,
-        langColor = repo.primaryLanguage.color,
-        pullRequests = prs.map { buildPullRequest(it) }
-    )
-}
-
-private fun buildPullRequest(pr: GitHubResponse.PullRequestNode): PullRequest {
-    return PullRequest(
-        title = pr.title,
-        state = pr.state,
-        number = pr.number,
-        url = pr.url,
-        additions = pr.additions,
-        deletions = pr.deletions,
-        createdAt = pr.createdAt,
-        closedAt = pr.closedAt
-    )
-}
-
 data class UserPullRequestsResponse(
     val owners: List<Owner>,
     val cursors: Cursors,
     val totalCount: Int
-)
+) {
+    companion object {
+        private const val GITHUB_BASE_URL = "https://github.com/"
 
-data class Owner(
-    val name: String,
-    val url: String,
-    val repositories: List<Repository>
-)
+        fun build(src: GitHubResponse): UserPullRequestsResponse {
+            val prs = src.data.user.pullRequests
+            return UserPullRequestsResponse(
+                owners = buildOwners(prs.nodes),
+                cursors = buildCursors(prs.edges),
+                totalCount = prs.totalCount
+            )
+        }
 
-data class Repository(
-    val name: String,
-    val description: String,
-    val url: String,
-    val watchers: Int,
-    val stars: Int,
-    val forks: Int,
-    val langName: String,
-    val langColor: String,
-    val pullRequests: List<PullRequest>
-)
+        private fun buildCursors(edges: List<GitHubResponse.Edge>): Cursors {
+            return Cursors(edges.first().cursor, edges.last().cursor)
+        }
 
-data class PullRequest(
-    val title: String,
-    val state: String,
-    val number: Int,
-    val url: String,
-    val additions: Int,
-    val deletions: Int,
-    val createdAt: String,
-    val closedAt: String?
-)
+        private fun buildOwners(nodes: List<GitHubResponse.PullRequestNode>): List<Owner> {
+            return nodes.groupBy {
+                it.repository.owner.login
+            }.map {
+                buildOwner(it.key, it.value)
+            }
+        }
 
-data class Cursors(
-    val first: String,
-    val last: String
-)
+        private fun buildOwner(name: String, prs: List<GitHubResponse.PullRequestNode>): Owner {
+            val url = GITHUB_BASE_URL + name
+            return Owner(
+                name,
+                url,
+                buildRepositories(prs, url)
+            )
+        }
+
+        private fun buildRepositories(prs: List<GitHubResponse.PullRequestNode>, ownerUrl: String): List<Repository> {
+            return prs.groupBy {
+                it.repository.name
+            }.map {
+                buildRepository(it.key, it.value, ownerUrl)
+            }
+        }
+
+        private fun buildRepository(name: String, prs: List<GitHubResponse.PullRequestNode>, ownerUrl: String): Repository {
+            val url = "$ownerUrl/$name"
+            val repo = prs.first().repository
+            return Repository(
+                name = name,
+                description = repo.description,
+                url = url,
+                watchers = repo.watchers.totalCount,
+                stars = repo.stargazers.totalCount,
+                forks = repo.forkCount,
+                langName = repo.primaryLanguage.name,
+                langColor = repo.primaryLanguage.color,
+                pullRequests = prs.map { buildPullRequest(it) }
+            )
+        }
+
+        private fun buildPullRequest(pr: GitHubResponse.PullRequestNode): PullRequest {
+            return PullRequest(
+                title = pr.title,
+                state = pr.state,
+                number = pr.number,
+                url = pr.url,
+                additions = pr.additions,
+                deletions = pr.deletions,
+                createdAt = pr.createdAt,
+                closedAt = pr.closedAt
+            )
+        }
+    }
+
+    data class Owner(
+        val name: String,
+        val url: String,
+        val repositories: List<Repository>
+    )
+
+    data class Repository(
+        val name: String,
+        val description: String,
+        val url: String,
+        val watchers: Int,
+        val stars: Int,
+        val forks: Int,
+        val langName: String,
+        val langColor: String,
+        val pullRequests: List<PullRequest>
+    )
+
+    data class PullRequest(
+        val title: String,
+        val state: String,
+        val number: Int,
+        val url: String,
+        val additions: Int,
+        val deletions: Int,
+        val createdAt: String,
+        val closedAt: String?
+    )
+
+    data class Cursors(
+        val first: String,
+        val last: String
+    )
+}
+
--- src/main/kotlin/github/prs/UserPullRequestsClient.kt
+++ src/main/kotlin/github/prs/UserPullRequestsClient.kt
@@ -1,14 +1,13 @@
 package com.github.lusingander.github.prs
 
 import com.github.lusingander.github.api.UserPullRequestsResponse
-import com.github.lusingander.github.api.buildUserResponseFrom
 import com.github.lusingander.github.GraphQLClient
 
 class UserPullRequestsClient(private val id: String): GraphQLClient() {
     fun request(): UserPullRequestsResponse {
         val query = UserPullRequests(this.id).query()
         val res = executePost<com.github.lusingander.github.prs.UserPullRequestsResponse>(query)
-        return buildUserResponseFrom(res)
+        return UserPullRequestsResponse.build(res)
     }
 }
 
